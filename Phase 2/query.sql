6dd1592f64f9a5c10cb40449a6a54a0e625a569f
Tyler <tylerlyjk@gmail.com>
2022-11-07 21:50:41 -0800

    Updated query.sql

--- Phase 2/query.sql
+++ Phase 2/query.sql
@@ -1,6 +1,6 @@
 SELECT "1---------";
 .headers off
-
+--Select regions targeted by client Universal Studios
 SELECT DISTINCT(region_name) 
 FROM region, client, requests, reqRegion
 WHERE client_name = 'UNIVERSAL STUDIOS'
@@ -11,7 +11,7 @@ AND rr_regionid = region_id;
 
 ---------------------
 SELECT "2---------";
-
+--Select Client with the biggest request budget
 SELECT client_name, max(request_budget)
 FROM client, requests
 WHERE client_id = request_clientid
@@ -21,7 +21,7 @@ GROUP BY client_name;
 ---------------------
 
 SELECT "3---------";
-
+--Total sum of duration for all videos made by team Social Stars
 SELECT sum(video_duration)
 FROM video, marketing, project
 WHERE team_id = project_teamId
@@ -31,7 +31,7 @@ AND team_name = 'SOCIAL STARS';
 ---------------------
 
 SELECT "4---------";
-
+--How many videos that target a region have the same language
 SELECT region_name, COUNT(video_id)
 FROM video, region
 WHERE video_language = region_language
@@ -41,7 +41,7 @@ GROUP BY region_name;
 ----------------------
 
 SELECT "5---------";
-
+--Average amount of views for every video platform
 SELECT video_platform, avg(video_views)
 FROM video
 GROUP BY video_platform;
@@ -49,7 +49,7 @@ GROUP BY video_platform;
 ----------------------
 
 SELECT "6---------";
-
+--Add a new video for project 2 that targets Europe and the language is Spanish
 INSERT INTO video(video_id, video_file, video_duration,
                 video_platform, video_views, video_regionId, 
                 video_demographicId,video_projectId, video_cost, video_language)
@@ -63,7 +63,7 @@ AND region_id = video_regionId;
 --------------------
 
 SELECT "7----------";
-
+--Find the the team with the most cost effective video being the ratio of (cost/views)
 SELECT team_name, max(video_cost/video_views) * 1.0 as Effectiveness
 FROM video, project , marketing
 WHERE video_projectId = project_Id
@@ -75,3 +75,49 @@ LIMIT 1;
 ---------------------
 
 SELECT "8-----------";
+--Delete projects where the cost went over the budget
+DELETE FROM project
+WHERE project_id in (SELECT project_id
+                    FROM project, requests
+                    WHERE project_cost > request_budget
+                    AND project_requestId = request_Id);
+
+SELECT * from project;
+
+---------------------
+
+SELECT "9------------";
+--Change budget for request 15 to 5000
+SELECT request_id, request_budget
+FROM requests
+WHERE request_id = 15;
+
+UPDATE requests
+SET request_budget = 5000
+WHERE request_id = 15;
+
+SELECT "Updated budget";
+SELECT request_id, request_budget
+FROM requests
+WHERE request_id = 15;
+
+---------------------
+
+SELECT "10-----------";
+--Change all client Id's for requests made by Universal Studios 
+--to Marvel Studios
+
+SELECT client_name, request_id, request_clientId
+FROM requests, client
+WHERE client_name = 'UNIVERSAL STUDIOS'
+AND request_clientid = client_id;
+
+UPDATE requests
+SET request_clientId = 2
+WHERE request_id = 1;
+
+SELECT "Updated Requests";
+SELECT client_name, request_id, request_clientId
+FROM requests, client
+WHERE client_name = 'MARVEL STUDIOS'
+AND request_clientid = client_id;
