d85b96c9835d242c5f3f5b5e7d80f9a10f06604b
Brandon <brandonkieng182@gmail.com>
2022-11-08 01:18:55 -0800

    sql statements

--- Phase 2/advertisements.py
+++ Phase 2/advertisements.py
@@ -113,7 +113,7 @@ def populateTable(_conn):
 def insert_client(_conn, _id, _name):
     try:
         sql = """INSERT INTO client(client_id, client_name) 
-        VALUES (?,?)"""
+        VALUES (?,?);"""
         args = [_id, _name]
         _conn.execute(sql, args)
         
@@ -123,38 +123,131 @@ def insert_client(_conn, _id, _name):
         _conn.rollback()
         print(e)
 
-def insert_marketing(_conn, _id, _name):
+def highest_view_platform(_conn):
     try:
-        sql = """INSERT INTO marketing(marketing_id, marketing_name) 
-        VALUES (?,?)"""
-        args = [_id, _name]
-        _conn.execute(sql, args)
-        
-        _conn.commit()
-        print("success")
+        sql = """SELECT v_videoPlatform
+                 FROM
+                 (
+                    SELECT v_videoPlatform, MAX(v_videoViews)
+                    FROM video
+                    GROUP BY v_videoPlatform
+                 );"""
+        cur = _conn.cursor()
+        cur.execute(sql)
+        l = '{:>10}'.format("Platform")
+        print(l)
+        for row in rows:
+            l = '{:>10}'.format(row[0])
+            print(l)
+
     except Error as e:
         _conn.rollback()
         print(e)
 
-def insert_requests(_conn, _id, _clientId, _budget):
+def num_videos_by_team_targeting_demographic(_conn, _team, _demo):
     try:
-        sql = """INSERT INTO requests(request_id, request_clientId, request_budget) 
-        VALUES (?,?,?)"""
-        args = [_id, _clientId, _budget]
-        _conn.execute(sql, args)
-        
-        _conn.commit()
-        print("success")
+        sql = """SELECT COUNT(v_videoId)
+                 FROM video, marketing, project, demographic
+                 WHERE m_teamName = ?
+                 AND m_teamID = p_teamId
+                 AND v_videoProjectId = p_projectId
+                 AND v_videoDemographicId = d_demographicId
+                 AND d_demographicName = ?
+                 """
+        args = [_team,_demo]
+        cur = _conn.cursor()
+        cur.execute(sql, args)
+        l = '{:>10}'.format("# of videos")
+        print(l)
+        for row in rows:
+            l = '{:>10}'.format(row[0])
+            print(l)
+
     except Error as e:
         _conn.rollback()
         print(e)
 
-def insert_project(_conn, _id, _teamId, _requestId, _cost):
+def regions_not_requested_by_two_clients(_conn, _clientA, _clientB):
     try:
+        sql = """SELECT r_regionName, r_regionId
+                 FROM region
+                 EXCLUDE
+                 SELECT r_regionName, r_regionId
+                 FROM region, request, reqregion, region
+                 WHERE c_clientName = ?
+                 AND c_clientId  = r_requestClientId
+                 AND r_requestId = rr_requestId
+                 AND rr_regionId = r_regionId
+                 EXCLUDE
+                 SELECT r_regionName, r_regionId
+                 WHERE c_clientName = ?
+                 AND c_clientId  = r_requestClientId
+                 AND r_requestId = rr_requestId
+                 AND rr_regionId = r_regionId
+                 """
+        args = [_clientA, _clientB]
+        cur = _conn.cursor()
+        cur.execute(sql, args)
+        l = '{:>10} {:>10}'.format("region", "ID")
+        print(l)
+
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:>10} {:>10}'.format(row[0],row[1])
+            print(l)
+
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def shortest_video_targeting_demo(_conn, _demographic):
+    try:
+        sql = """SELECT v_videoId, v_videoFile, MIN(v_videoDuration)
+                FROM video, demographic
+                WHERE d_demographic = ?
+                AND d_demographicId = v_videoDemographicId;
+                 """
+        args = [_demographic]
+        cur = _conn.cursor()
+        cur.execute(sql, args)
+        l = '{:>10} {:>10} {:>10}'.format("Video ID", "File name", "Duration")
+        print(l)
+
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:>10} {:>10} {:>10}'.format(row[0],row[1],row[2])
+            print(l)
+
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def new_project_with_marketing_request(_conn, _team,_request):
+    try:
+        #find ID
+        sql = """SELECT MAX(p_projectId) FROM project"""
+        cur = _conn.cursor()
+        cur.execute(sql)
+    
+        rows = cur.fetchall()
+    
+        for row in rows:
+            newId = row[0]+1;
+        
+        #find teamID
+        sql = """SELECT m_teamId FROM marketing WHERE ?"""
+        args = [_team]
+        cur = _conn.cursor()
+        cur.execute(sql, args)
+        rows = cur.fetchall()
+    
+        for row in rows:
+            teamId = row[0]+1;
+        
         sql = """INSERT INTO project(project_id, project_teamId,
                                 project_requestId, project_cost) 
-        VALUES (?,?,?,?)"""
-        args = [_id, _teamId, _requestId, _cost]
+        VALUES (?,?,?,?);"""
+        args = [newId, teamId, _request, 0]
         _conn.execute(sql, args)
         
         _conn.commit()
@@ -163,72 +256,132 @@ def insert_project(_conn, _id, _teamId, _requestId, _cost):
         _conn.rollback()
         print(e)
 
-def insert_video(_conn, _id, _file, _duration, _platform, _views,
-                _language, _cost, _regionId, _demographicId):
+def total_projects_by_team(_conn, _team):
     try:
-        sql = """INSERT INTO video(video_id, video_file, video_duration,
-                                   video_platform, video_views, video_language,
-                                   video_cost, video_regionId, video_demographicId) 
-        VALUES (?,?,?,?,?,?,?,?,?)"""
-        args = [_id, _file, _duration, _platform, _views,
-                _language, _cost, _regionId, _demographicId]
-        _conn.execute(sql, args)
-        
-        _conn.commit()
-        print("success")
+        sql = """SELECT COUNT(p_projectId)
+                FROM project, marketing
+                WHERE m_teamId = p_teamId
+                AND m_teamName = ?
+                 """
+        args = [_team]
+        cur = _conn.cursor()
+        cur.execute(sql, args)
+        l = '{:>10}'.format("Total")
+        print(l)
+
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:>10}'.format(row[0])
+            print(l)
+
     except Error as e:
         _conn.rollback()
         print(e)
 
-def insert_region(_conn, _id, _name, _language):
+def demographic_with_highest_views(_conn):
     try:
-        sql = """INSERT INTO region(reigon_id, region_name, region_language) 
-        VALUES (?,?,?)"""
-        args = [_id, _name, _language]
-        _conn.execute(sql, args)
-        
-        _conn.commit()
-        print("success")
+        sql = """SELECT d_demographicName, MAX(v_videoViews)
+                 FROM video, demographic
+                 WHERE v_videoDemoGraphicId = d_demographicId
+                 GROUP BY d_demographicId
+                 """
+        cur = _conn.cursor()
+        cur.execute(sql)
+        l = '{:>10} {:>10}'.format("Demographic", "views")
+        print(l)
+
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:>10} {:>10}'.format(row[0], row[1])
+            print(l)
+
     except Error as e:
         _conn.rollback()
         print(e)
 
-
-def insert_demographic(_conn, _id, _name):
+def new_reqDemo(_conn,_demographic,_request):
     try:
-        sql = """INSERT INTO demographic(demographic_id, demographic_name) 
-        VALUES (?,?)"""
-        args = [_id, _name]
-        _conn.execute(sql, args)
-        
-        _conn.commit()
-        print("success")
-    except Error as e:
-        _conn.rollback()
-        print(e)
+        sql = """SELECT d_demographicName
+                 FROM demographic
+                 WHERE d_demographicName = ?
+                 """
+        args = [_demographic]
+        cur = _conn.cursor()
+        cur.execute(sql, args)
 
-def insert_reqDemo(_conn, _requestId, _demographicId):
-    try:
+        rows = cur.fetchall()
+
+        for row in rows:
+            demod = row[0]
+
         sql = """INSERT INTO reqDemo(rd_requestId, rd_demographicId) 
-        VALUES (?,?)"""
-        args = [_requestId, _demographicId]
+        VALUES (?,?);"""
+        args = [_request, demoId]
         _conn.execute(sql, args)
         
         _conn.commit()
         print("success")
+
     except Error as e:
         _conn.rollback()
         print(e)
 
-def insert_reqRegion(_conn, _requestId, _regionId):
+def remove_marketing_and_projects(_conn, _team):
     try:
-        sql = """INSERT INTO reqRegion(rr_requestId, rr_regionId) 
-        VALUES (?,?)"""
-        args = [_requestId, _regionId]
+        sql = """SELECT m_teamId
+                 FROM marketing
+                 WHERE m_teamName = ?
+                 """
+        args = [_team]
+        cur = _conn.cursor()
+        cur.execute(sql, args)
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            teamId = row[0]
+
+        sql = """DELETE FROM projects WHERE p_teamId = ?"""
+                 """
+        args = [teamId]
         _conn.execute(sql, args)
         
         _conn.commit()
-        print("success")
+
+       sql = """DELETE FROM marketing WHERE m_teamName = ?"""
+                 """
+        args = [_team]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def projects_dont_have_all_requested_regions(_conn):
+    try:
+        sql = """SELECT DISTINCT p_projectId
+                 FROM
+                 (
+                     SELECT rr_regionId, p_projectId
+                     FROM reqregion, project
+                     WHERE rr_requestId = p_requestId
+                     EXCLUDE
+                     SELECT v_videoRegionId, p_projectId
+                     FROM project, video
+                     WHERE v_videoProjectId = p_projectId
+                 );
+                 """
+        cur = _conn.cursor()
+        cur.execute(sql)
+        l = '{:>10}'.format("projects")
+        print(l)
+
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:>10}'.format(row[0])
+            print(l)
+
     except Error as e:
         _conn.rollback()
         print(e)
---
+++ Phase 2/functions.py
@@ -0,0 +1,335 @@
+def insert_marketing(_conn, _id, _name):
+    try:
+        sql = """INSERT INTO marketing(marketing_id, marketing_name) 
+        VALUES (?,?);"""
+        args = [_id, _name]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def insert_requests(_conn, _id, _clientId, _budget):
+    try:
+        sql = """INSERT INTO requests(request_id, request_clientId, request_budget) 
+        VALUES (?,?,?);"""
+        args = [_id, _clientId, _budget]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def insert_project(_conn, _id, _teamId, _requestId, _cost):
+    try:
+        sql = """INSERT INTO project(project_id, project_teamId,
+                                project_requestId, project_cost) 
+        VALUES (?,?,?,?);"""
+        args = [_id, _teamId, _requestId, _cost]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def insert_video(_conn, _id, _file, _duration, _platform, _views,
+                _language, _cost, _regionId, _demographicId):
+    try:
+        sql = """INSERT INTO video(video_id, video_file, video_duration,
+                                   video_platform, video_views, video_language,
+                                   video_cost, video_regionId, video_demographicId) 
+        VALUES (?,?,?,?,?,?,?,?,?);"""
+        args = [_id, _file, _duration, _platform, _views,
+                _language, _cost, _regionId, _demographicId]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def insert_region(_conn, _id, _name, _language):
+    try:
+        sql = """INSERT INTO region(reigon_id, region_name, region_language) 
+        VALUES (?,?,?);"""
+        args = [_id, _name, _language]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+
+def insert_demographic(_conn, _id, _name):
+    try:
+        sql = """INSERT INTO demographic(demographic_id, demographic_name) 
+        VALUES (?,?);"""
+        args = [_id, _name]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def insert_reqDemo(_conn, _requestId, _demographicId):
+    try:
+        sql = """INSERT INTO reqDemo(rd_requestId, rd_demographicId) 
+        VALUES (?,?);"""
+        args = [_requestId, _demographicId]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def insert_reqRegion(_conn, _requestId, _regionId):
+    try:
+        sql = """INSERT INTO reqRegion(rr_requestId, rr_regionId) 
+        VALUES (?,?);"""
+        args = [_requestId, _regionId]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def update_client(_conn, _id, _name):
+    try:
+        sql = """UPDATE client SET c_clientName = ? WHERE client_id = ?;"""
+        args = [_name,_id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def update_marketing(_conn, _id, _name):
+    try:
+        sql = """UPDATE marketing SET m_marketingName = ? WHERE m_marketingId = ?;"""
+        args = [_name, _id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def update_requests(_conn, _id, _clientId, _budget):
+    try:
+        sql = """UPDATE requests SET request_clientId = ?, request_budget = ? WHERE request_id = ?;"""
+        args = [_clientId, _budget, _id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def update_project(_conn, _id, _teamId, _requestId, _cost):
+    try:
+        sql = """UPDATE project SET project_teamId = ?,
+                 project_requestId = ?, project_cost = ? WHERE project_id = ?;"""
+        args = [_teamId, _requestId, _cost, _id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def insert_video(_conn, _id, _file, _duration, _platform, _views,
+                _language, _cost, _regionId, _demographicId):
+    try:
+        sql = """UPDATE video SET video_file = ?, video_duration = ?,
+                                   video_platform = ?, video_views = ?, video_language = ?,
+                                   video_cost = ?, video_regionId = ?, video_demographicId = ?
+                              WHERE video_id = ?;"""
+        args = [_file, _duration, _platform, _views,
+                _language, _cost, _regionId, _demographicId, _id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def update_region(_conn, _id, _name, _language):
+    try:
+        sql = """UPDATE region SET region_name = ?, region_language = ? WHERE reigon_id = ?;"""
+        args = [_name, _language, _id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+
+def update_demographic(_conn, _id, _name):
+    try:
+        sql = """UPDATE demographic SET demographic_name = ? WHERE demographic_id = ?;""" 
+        args = [_name, _id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def update_reqRegion(_conn, _requestId, _regionId, new_requestId, new_regionId):
+    try:
+        sql = """UPDATE reqREgion SET rd_requestId = ?, rd_regionId = ?
+        WHERE rd_requestId = ? AND rd_regionId;"""
+        args = [new_requestId, new_regionId, _requestId, _regionId]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def update_reqDemo(_conn, _requestId, _demographicId, new_requestId, new_demographicId):
+    try:
+        sql = """UPDATE reqDemo SET rd_requestId = ?, rd_demographicId = ?
+        WHERE rd_requestId = ? AND rd_demographicId;"""
+        args = [new_requestId,new_demographicId, _requestId, _demographicId]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def delete_client(_conn, _id):
+    try:
+        sql = """DELETE FROM client WHERE client_id = ?;"""
+        args = [_id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def delete_marketing(_conn, _id):
+    try:
+        sql = """DELETE FROM marketing WHERE marketing_id =?;"""
+        args = [_id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def delete_requests(_conn, _id):
+    try:
+        sql = """DELETE FROM requests WHERE request_id = ?;""" 
+        args = [_id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def delete_project(_conn, _id):
+    try:
+        sql = """DELETE FROM project WHERE project_id = ?;"""
+        args = [_id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def delete_video(_conn, _id):
+    try:
+        sql = """DELETE FROM video WHERE video_id = ?;""" 
+        args = [_id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def delete_region(_conn, _id):
+    try:
+        sql = """DELETE FROM region WHERE reigon_id = ?;"""
+        args = [_id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+
+def delete_demographic(_conn, _id):
+    try:
+        sql = """DELETE FROM demographic WHERE demographic_id = ?;"""
+        args = [_id]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def delete_reqDemo(_conn, _requestId, _demographicId):
+    try:
+        sql = """DELETE FROM reqDemo WHERE rd_requestId = ? AND rd_demographicId = ?;""" 
+        args = [_requestId, _demographicId]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+def delete_reqRegion(_conn, _requestId, _regionId):
+    try:
+        sql = """DELETE FROM reqRegion WHERE rr_requestId = ? AND rr_regionId = ?;"""
+        args = [_requestId, _regionId]
+        _conn.execute(sql, args)
+        
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
